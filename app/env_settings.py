c9128a07900b1ed3d1d347ea17c4fba5bc94a6d3
NV <Nikita.Voskresenskiy@phystech.edu>
2025-11-29 21:20:54 +0300

    some changes to build and run with docker without docker-compose

--- app/env_settings.py
+++ app/env_settings.py
@@ -1,11 +1,18 @@
 from pydantic_settings import BaseSettings
+import os
+
 class EnvSettings(BaseSettings):
     BOT_TOKEN: str  
     BOT_ADMIN_ID: int
     CHANNEL_ID: int # Private channel ID (negative for channels)
     CHANNEL_USERNAME: str # admin ID
+    LOG_LEVEL: str = "ERROR"  # Default to INFO
 
     class Config:
-        env_file = ".env"
+        env_file = ".env" if os.getenv("ENVIRONMENT") == "dev" else None
 
-env = EnvSettings()
+# Создаем конфиг (будет читать из переменных окружения)
+try:
+    env = EnvSettings()
+except Exception as e:
+    exit(1)
--- app/logger_settings.py
+++ app/logger_settings.py
@@ -1,4 +1,10 @@
 import logging
+import sys
+from env_settings import env
 # Configure logging
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(
+    level=getattr(logging, env.LOG_LEVEL.upper(), logging.ERROR),
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)]  # Важно: stdout, а не stderr
+)
 logger = logging.getLogger(__name__)
